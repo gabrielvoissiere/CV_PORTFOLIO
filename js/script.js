8e41491f9528f5902b983c53c143f570d9595fc9
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2021-02-17 12:58:46 +0100

    menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
-
+    $(".burger").click(function () { 
+        $("nav").toggleClass("slide") 
+        $(".l2-1").toggleClass("rotleft")
+        $(".l2-2").toggleClass("rotright")  
+        $(".l1, .l3").toggleClass("hide")
+        $("body").toggleClass("stop")
+    });
 
 })
