3aea857409434217af0fd9c727824bbf5290b13d
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2021-03-03 10:59:20 +0100

    fin

--- js/script.js
+++ js/script.js
@@ -1,45 +1,98 @@
-$(document).ready(function(){
+// animation du burger et apparition du menu
+document.querySelector(".burger").addEventListener('click', function () {
+    document.querySelector("nav").classList.toggle("slide")
+    document.querySelector(".l2-1").classList.toggle("rotleft")
+    document.querySelector(".l2-2").classList.toggle("rotright")
+    document.querySelector(".l1").classList.toggle("hide")
+    document.querySelector(".l3").classList.toggle("hide")
+    document.querySelector("body").classList.toggle("stop")
+})
 
-    // animation du burger et volet menu
-    $(".burger").click(function () { 
-        $("nav").toggleClass("slide") 
-        $(".l2-1").toggleClass("rotleft")
-        $(".l2-2").toggleClass("rotright")  
-        $(".l1, .l3").toggleClass("hide")
-        $("body").toggleClass("stop")
-    });
+// boutton filtre
+document.querySelector(".all").addEventListener('click', function () {
+    document.querySelector(".all").classList.add("select")
+    document.querySelector(".perso").classList.remove("select")
+    document.querySelector(".pro").classList.remove("select")
+    document.querySelector(".sco").classList.remove("select")
 
-    // bouton filtre des page realisations
-    $(".all").click(function () { 
-        $(this).siblings().removeClass("select")
-        $(this).addClass("select")
-        $(".scolaire, .professionel, .personnel").css("display", "block")
-    });
+    var personnel = document.querySelectorAll(".personnel");
+    for (let i = 0; i < personnel.length; ++i) {
+        personnel[i].style.display = "block";
+    }
 
-    $(".perso").click(function () { 
-        $(this).siblings().removeClass("select")
-        $(this).addClass("select")
-        $(".personnel").css("display", "block")
-        $(".scolaire, .professionel").css("display", "none")
-    });
+    var professionel = document.querySelectorAll(".professionel");
+    for (let i = 0; i < professionel.length; ++i) {
+        professionel[i].style.display = "block";
+    }
 
-    $(".pro").click(function () { 
-        $(this).siblings().removeClass("select")
-        $(this).addClass("select")
-        $(".professionel").css("display", "block")
-        $(".scolaire, .personnel").css("display", "none")
-    });
+    var scolaire = document.querySelectorAll(".scolaire");
+    for (let i = 0; i < scolaire.length; ++i) {
+        scolaire[i].style.display = "block";
+    }
+})
 
-    $(".sco").click(function () { 
-        $(this).siblings().removeClass("select")
-        $(this).addClass("select")
-        $(".scolaire").css("display", "block")
-        $(".professionel, .personnel").css("display", "none")
-    });
+document.querySelector(".perso").addEventListener('click', function () {
+    document.querySelector(".all").classList.remove("select")
+    document.querySelector(".perso").classList.add("select")
+    document.querySelector(".pro").classList.remove("select")
+    document.querySelector(".sco").classList.remove("select")
 
-    // validation du formulaire
-        if ($("#name") == "") {
-            $("#caution").text("Veuillez remplir tous les champs")
-            $("#submit");addClass("disabled")
-        }
+    var personnel = document.querySelectorAll(".personnel");
+    for (let i = 0; i < personnel.length; ++i) {
+        personnel[i].style.display = "block";
+    }
+
+    var professionel = document.querySelectorAll(".professionel");
+    for (let i = 0; i < professionel.length; ++i) {
+        professionel[i].style.display = "none";
+    }
+
+    var scolaire = document.querySelectorAll(".scolaire");
+    for (let i = 0; i < scolaire.length; ++i) {
+        scolaire[i].style.display = "none";
+    }
+})
+
+document.querySelector(".pro").addEventListener('click', function () {
+    document.querySelector(".all").classList.remove("select")
+    document.querySelector(".pro").classList.add("select")
+    document.querySelector(".perso").classList.remove("select")
+    document.querySelector(".sco").classList.remove("select")
+
+    var personnel = document.querySelectorAll(".personnel");
+    for (let i = 0; i < personnel.length; ++i) {
+        personnel[i].style.display = "none";
+    }
+
+    var professionel = document.querySelectorAll(".professionel");
+    for (let i = 0; i < professionel.length; ++i) {
+        professionel[i].style.display = "block";
+    }
+
+    var scolaire = document.querySelectorAll(".scolaire");
+    for (let i = 0; i < scolaire.length; ++i) {
+        scolaire[i].style.display = "none";
+    }
+})
+
+document.querySelector(".sco").addEventListener('click', function () {
+    document.querySelector(".all").classList.remove("select")
+    document.querySelector(".sco").classList.add("select")
+    document.querySelector(".perso").classList.remove("select")
+    document.querySelector(".pro").classList.remove("select")
+
+    var personnel = document.querySelectorAll(".personnel");
+    for (let i = 0; i < personnel.length; ++i) {
+        personnel[i].style.display = "none";
+    }
+
+    var professionel = document.querySelectorAll(".professionel");
+    for (let i = 0; i < professionel.length; ++i) {
+        professionel[i].style.display = "none";
+    }
+
+    var scolaire = document.querySelectorAll(".scolaire");
+    for (let i = 0; i < scolaire.length; ++i) {
+        scolaire[i].style.display = "block";
+    }
 })
